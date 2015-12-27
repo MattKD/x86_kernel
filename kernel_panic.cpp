b457ab7a23f3c2556f93c6582f8dff4266a21b5a
Matt Donovan <mkd1219@gmail.com>
2015-12-27 15:55:34 -0500

    Added variable args and format string to kernel_panic

--- kernel_panic.cpp
+++ kernel_panic.cpp
@@ -4,9 +4,12 @@ extern "C" {
 
 void kernel_halt(); // loader.s
 
-void kernel_panic(const char *msg)
+void kernel_panic(const char *fmt, ...)
 {
-  puts(msg);
+  va_list args;
+  va_start(args, fmt);
+  vprintf(fmt, args);
+  va_end(args);
   kernel_halt();
 }
 
--- kernel_panic.h
+++ kernel_panic.h
@@ -1,6 +1,6 @@
 #ifndef KERNEL_PANIC_H
 #define KERNEL_PANIC_H
 
-extern "C" void kernel_panic(const char *msg);
+extern "C" void kernel_panic(const char *fmt, ...);
 
 #endif

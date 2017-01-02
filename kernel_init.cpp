9b224f8cb8ca8a110b9f7f064046f629d6c98f94
Matt Donovan <mkd1219@gmail.com>
2017-01-02 16:41:47 -0500

    Fixed bug: allocFrame could return kernel memory

--- kernel_init.cpp
+++ kernel_init.cpp
@@ -34,6 +34,8 @@ void freeFrame(void *frame)
 
 } // end namespace kernel
 
+bool allocFrameInit(uint32_t mb_magic, multiboot_info_t *mb_info);
+
 extern "C"
 void kernelInit(uint32_t mb_magic, multiboot_info_t *mb_info)
 {
@@ -52,39 +54,63 @@ void kernelInit(uint32_t mb_magic, multiboot_info_t *mb_info)
   printf("kernel start = %x\n", &_kernel_start_addr);
   printf("kernel end = %x\n", &_kernel_end_addr);
 
-  if (mb_magic == MULTIBOOT_BOOTLOADER_MAGIC) {
-    multiboot_memory_map_t *mmap = 
-      (multiboot_memory_map_t *) mb_info->mmap_addr;
-    multiboot_memory_map_t *mmap_end = (multiboot_memory_map_t *)
-      (mb_info->mmap_addr + mb_info->mmap_length);
-
-    while(mmap < mmap_end) {
-      printf("address: %x, ", mmap->addr);
-      printf("length: %x, ", mmap->len);
-      printf("type: %s\n", mmap->type == 1 ? "Available" : "Reserved");
-
-      if (mmap->type == 1) {
-        uintptr_t frame = (uintptr_t)mmap->addr;
-        uintptr_t mem_end = (uintptr_t)mmap->addr + mmap->len;
-        // align frame on 4kb boundary
-        if (frame % 4096 != 0) {
-          frame += 4096;
-          frame &= ~4095;
-        }
-
-        while (frame+4095 < mem_end && num_free_frames < max_num_frames) {
-          frames[num_free_frames++] = (void*)frame;
-          frame += 4096;
-        }
-      }
-
-      mmap = (multiboot_memory_map_t *) 
-        ((uint32_t)mmap + mmap->size + sizeof(mmap->size));
-    }
-    
+  if (allocFrameInit(mb_magic, mb_info)) {
     printf("Number of 4KB frames: %d\n", num_free_frames);
     enablePaging();
   } else {
-    printf("Error: Couldn't initialize frame allocator\n");
+    kernel_panic("Error: Couldn't initialize frame allocator\n");
   }
+}
+
+static inline
+uintptr_t align4K(uintptr_t addr)
+{
+  if (addr % 4096 != 0) {
+    addr += 4096;
+    addr &= ~4095;
+  }
+  return addr;
+}
+
+bool allocFrameInit(uint32_t mb_magic, multiboot_info_t *mb_info)
+{
+  if (mb_magic != MULTIBOOT_BOOTLOADER_MAGIC) {
+    return false;
+  }
+
+  multiboot_memory_map_t *mmap = 
+    (multiboot_memory_map_t *) mb_info->mmap_addr;
+  multiboot_memory_map_t *mmap_end = (multiboot_memory_map_t *)
+    (mb_info->mmap_addr + mb_info->mmap_length);
+
+  while(mmap < mmap_end) {
+    printf("address: %x, ", mmap->addr);
+    printf("length: %x, ", mmap->len);
+    printf("type: %s\n", mmap->type == 1 ? "Available" : "Reserved");
+
+    if (mmap->type == 1) {
+      uintptr_t frame = (uintptr_t)mmap->addr;
+      uintptr_t mem_end = (uintptr_t)mmap->addr + mmap->len;
+      // align frame on 4kb boundary
+      frame = align4K(frame);
+
+      while (frame+4095 < mem_end && num_free_frames < max_num_frames) {
+        // skip any memory within kernel or 1st MB
+        if (frame < (uintptr_t)&_kernel_end_addr) {
+          printf("within kernel 2\n");
+          frame = (uintptr_t)&_kernel_end_addr;
+          frame = align4K(frame);
+          continue;
+        }
+
+        frames[num_free_frames++] = (void*)frame;
+        frame += 4096;
+      }
+    }
+
+    mmap = (multiboot_memory_map_t *) 
+      ((uint32_t)mmap + mmap->size + sizeof(mmap->size));
+  }
+  
+  return true;
 }
